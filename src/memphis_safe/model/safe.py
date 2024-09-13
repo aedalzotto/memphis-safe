a6ee20432fc6de57a1f361c35ac37cdcf384f0ff
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-09-13 17:40:03 -0300

    Export test report

--- src/memphis_safe/model/safe.py
+++ src/memphis_safe/model/safe.py
@@ -1,10 +1,12 @@
 from xgboost import XGBRegressor
-from pandas import read_csv
+from pandas import read_csv, DataFrame, concat
 from yaspin import yaspin
 from sklearn.metrics import recall_score, precision_score, f1_score
 
 class Safe:
     def __init__(self, model, test):
+        self.model_name = model
+        self.test_name  = test
         with yaspin(text="Loading model...") as spinner:
             self.model = XGBRegressor()
             self.model.load_model(model)
@@ -17,15 +19,26 @@ class Safe:
     
     def test(self, threshold):
         print("\n", end="")
+        y_pred = DataFrame()
         with yaspin(text="Testing model...") as spinner:
-            self.y = self.X[["anomaly", "total_time"]]
-            self.X.drop(columns=["anomaly", "total_time"], inplace=True)
+            self.y = self.X[["total_time", "anomaly"]]
+            self.X.drop(columns=["total_time", "anomaly"], inplace=True)
 
-            y_pred = self.model.predict(self.X)
+            y_pred["time_pred"] = self.model.predict(self.X)
             tag = self.y["anomaly"].astype('bool')
-            tag_pred = abs(y_pred - self.y["total_time"]) / self.y["total_time"] > threshold
+            y_pred["anomaly_pred"] = abs(y_pred["time_pred"] - self.y["total_time"]) / self.y["total_time"] > threshold
             spinner.ok()
 
-        print("\nTest recall:    {}".format(   recall_score(tag, tag_pred)))
-        print(  "Test precision: {}".format(precision_score(tag, tag_pred)))
-        print(  "Test F1:        {}".format(       f1_score(tag, tag_pred)))
+        print("\nTest recall:    {}".format(   recall_score(self.y["anomaly"], y_pred["anomaly_pred"])))
+        print(  "Test precision: {}".format(precision_score(self.y["anomaly"], y_pred["anomaly_pred"])))
+        print(  "Test F1:        {}".format(       f1_score(self.y["anomaly"], y_pred["anomaly_pred"])))
+
+        with yaspin(text="Exporting report...") as spinner:
+            test_path = "_".join(self.test_name.split(".")[-2].split("_")[:-1])
+            model_params = "_".join(self.model_name.split("_")[-3:-1])
+            name = "{}_{}_tested.csv".format(test_path, model_params)
+            df = concat([self.X, self.y, y_pred], axis=1)
+            df.to_csv(name, index=False)
+            spinner.ok()
+
+        print("Report exported to {}".format(name))
