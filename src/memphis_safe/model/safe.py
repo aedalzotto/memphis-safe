adbabec73f8fdd6e25ec10f44b414fd9ec01aee8
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2025-04-10 19:03:11 -0300

    Add more insight to testing

--- src/memphis_safe/model/safe.py
+++ src/memphis_safe/model/safe.py
@@ -14,34 +14,50 @@ class Safe:
 
         print("\n", end="")
         with yaspin(text="Loading test dataset...") as spinner:
-            self.X         = read_csv(test)
-            self.y         = self.X[["latency", "malicious"]]
-            self.X         = self.X[["rel_time", "prod", "cons", "hops", "size"]]
-            self.X["prod"] = self.X["prod"].astype("category")
-            self.X["cons"] = self.X["cons"].astype("category")
+            self.df         = read_csv(test)
+            self.X         = self.df[["rel_time", "prod", "cons", "hops", "size"]]
+            self.X.loc[:,"prod"] = self.X["prod"].astype("category")
+            self.X.loc[:,"cons"] = self.X["cons"].astype("category")
             self.X         = get_dummies(self.X, columns=["prod", "cons"])
             spinner.ok()
+
+        print("\nFeature importances:")
+        print(self.model.feature_importances_)
     
     def test(self, threshold):
         print("\n", end="")
         y_pred = DataFrame()
         with yaspin(text="Testing model...") as spinner:
             y_pred["lat_pred"] = self.model.predict(self.X)
-            y_pred["mal_pred"] = (self.y["latency"] - y_pred["lat_pred"]) / y_pred["lat_pred"] > threshold
+            y_pred["lat_diff"] = (self.df["latency"] - y_pred["lat_pred"]) / y_pred["lat_pred"]
+            y_pred["mal_pred"] = y_pred["lat_diff"] > threshold
             spinner.ok()
 
-        print("\nTest recall:    {}".format(   recall_score(self.y["malicious"], y_pred["mal_pred"], zero_division=1.0)))
-        print(  "Test precision: {}".format(precision_score(self.y["malicious"], y_pred["mal_pred"])))
-        print(  "Test F1:        {}".format(       f1_score(self.y["malicious"], y_pred["mal_pred"])))
+        print("\nTest recall:    {}".format(   recall_score(self.df["malicious"], y_pred["mal_pred"])))
+        print(  "Test precision: {}".format(precision_score(self.df["malicious"], y_pred["mal_pred"])))
+        print(  "Test F1:        {}".format(       f1_score(self.df["malicious"], y_pred["mal_pred"])))
 
-        print(confusion_matrix(self.y["malicious"], y_pred["mal_pred"]))
+        print(confusion_matrix(self.df["malicious"], y_pred["mal_pred"], labels=[True, False]))
+
+        self.df = concat([self.df, y_pred], axis=1)
+        print("\nMin. diff TP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == True)]["lat_diff"].min()))
+        print(  "Max. diff TP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == True)]["lat_diff"].max()))
+        print(  "Avg. diff TP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == True)]["lat_diff"].mean()))
+
+        print("\nMin. diff FP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == False)]["lat_diff"].min()))
+        print(  "Max. diff FP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == False)]["lat_diff"].max()))
+        print(  "Avg. diff FP:   {}".format(self.df[(self.df["mal_pred"] == True) & (self.df["malicious"] == False)]["lat_diff"].mean()))
+
+        print("\nMin. HT FN:     {}".format(self.df[(self.df["mal_pred"] == False) & (self.df["malicious"] == True)]["mal_cycles"].min()))
+        print(  "Max. HT FN:     {}".format(self.df[(self.df["mal_pred"] == False) & (self.df["malicious"] == True)]["mal_cycles"].max()))
+        print(  "Avg. HT FN:     {}".format(self.df[(self.df["mal_pred"] == False) & (self.df["malicious"] == True)]["mal_cycles"].mean()))
+
 
         with yaspin(text="Exporting report...") as spinner:
             test_path = "_".join(self.test_name.split(".")[-2].split("_")[:-1])
             model_params = "_".join(self.model_name.split("_")[-3:-1])
             name = "{}_{}_tested.csv".format(test_path, model_params)
-            df = concat([self.X, self.y, y_pred], axis=1)
-            df.to_csv(name, index=False)
+            self.df.to_csv(name, index=False)
             spinner.ok()
 
         print("Report exported to {}".format(name))
