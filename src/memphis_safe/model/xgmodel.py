e2585b2c15e3b5099a5338248517e0e19b778bd3
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2025-07-17 09:24:54 -0300

    Remove initial weights and change to RMSE

--- src/memphis_safe/model/xgmodel.py
+++ src/memphis_safe/model/xgmodel.py
@@ -14,19 +14,6 @@ class XGModel:
         with yaspin(text="Loading train dataset...") as spinner:
             self.X         = read_csv(name)
 
-            # self.X.loc[self.X[self.X["latency"] < 100].index, "latency"] = 140
-            # self.X.sort_values(by='rel_time', inplace = True)
-
-            total = self.X.shape[0]
-            warmup = self.X[self.X["rel_time"] < 1000].index
-            # small = self.X[self.X["latency"] < 50].index
-            weight = int(total/len(warmup)/2)
-            # weight = 1
-            self.X["weight"] = 1.0
-            self.X.loc[warmup, "weight"] = weight
-            # self.X.loc[small, "weight"] = 0.01
-            self.weights = self.X["weight"].values
-            
             self.y         = self.X[["latency"]]
             self.X         = self.X[["rel_time", "prod", "cons", "hops", "size"]]
             self.X["prod"] = self.X["prod"].astype("category")
@@ -34,77 +21,70 @@ class XGModel:
             self.X         = get_dummies(self.X, columns=["prod", "cons"])
             spinner.ok()
 
-        print("\n{} total".format(total))
-        print("{} with more weight".format(len(warmup)))
-        print("{} weight".format(weight))
-
-    def __get_mape(self, cv_k, n_estimators, max_depth):
+    def __get_score(self, cv_k, n_estimators, max_depth):
         # model = XGBRegressor(base_score=50, n_estimators=n_estimators, max_depth=max_depth, min_child_weight=5, gamma=1, reg_lambda=1, subsample=0.8, colsample_bytree=0.8, eta=0.2)
-        model = XGBRegressor(base_score=50, n_estimators=n_estimators, max_depth=max_depth)
-        scores = cross_val_score(model, self.X, self.y, scoring=neg_mean_percentage_error, cv=cv_k, params={'sample_weight': self.weights})
-        # scores = cross_val_score(model, self.X, self.y, scoring=neg_mean_percentage_error, cv=cv_k)
-        mape = -scores
-        return mape, model
+        model = XGBRegressor(base_score=50, n_estimators=n_estimators, max_depth=max_depth, objective='reg:squarederror')
+        scores = cross_val_score(model, self.X, self.y, scoring='neg_root_mean_squared_error', cv=cv_k)
+        score = -scores
+        return score, model
 
     def train(self, cv_k, n_estimators=None, max_depth=None):
-        last_good_n_estimators = 100
-        last_good_max_depth = 6
-        if n_estimators is not None:
-            last_good_n_estimators = n_estimators
-        if max_depth is not None:
-            last_good_max_depth = max_depth
+        last_good_n_estimators = 100 if n_estimators is None else n_estimators
+        last_good_max_depth = 6 if max_depth is None else max_depth
         print("\n", end="")
         with yaspin(text="Training base model...") as spinner:
-            last_good_score, last_good_model = self.__get_mape(cv_k, last_good_n_estimators, last_good_max_depth)
+            last_good_score, last_good_model = self.__get_score(cv_k, last_good_n_estimators, last_good_max_depth)
             spinner.ok()
 
-        print("Base cross-validation mean MAPE: {}".format(round(last_good_score.mean(), 3)))
-        print("Base cross-validation MAPE std dev: {}".format(round(last_good_score.std(), 3)))
+        print("Base cross-validation mean RMSE: {}".format(round(last_good_score.mean(), 3)))
+        print("Base cross-validation RMSE std dev: {}".format(round(last_good_score.std(), 3)))
         print("{}-fold base cross-validation scores:".format(cv_k))
         print(last_good_score)
 
-        with yaspin(text="Finding smallest model...") as spinner:
-            max_score = last_good_score.mean() * 1.1
+        if n_estimators is None and max_depth is None:
+            with yaspin(text="Finding smallest model...") as spinner:
+                max_score = last_good_score.mean() * 1.1
 
-            last_bad_n_estimators = 0
-            while True:
-                n_estimators = round((last_good_n_estimators+last_bad_n_estimators) / 2)
-                if n_estimators in [last_good_n_estimators, last_bad_n_estimators]:
-                    break
-                print("Trying {} estimators".format(n_estimators))
-                mape, model = self.__get_mape(cv_k, n_estimators, last_good_max_depth)
-                last_score = mape.mean()
-                if last_score < max_score:
-                    last_good_model = model
-                    last_good_score = mape
-                    last_good_n_estimators = n_estimators
-                else:
-                    last_bad_n_estimators = n_estimators
+                last_bad_n_estimators = 0
+                while True:
+                    n_estimators = round((last_good_n_estimators+last_bad_n_estimators) / 2)
+                    if n_estimators in [last_good_n_estimators, last_bad_n_estimators]:
+                        break
+                    print("Trying {} estimators".format(n_estimators))
+                    score, model = self.__get_score(cv_k, n_estimators, last_good_max_depth)
+                    last_score = score.mean()
+                    if last_score < max_score:
+                        last_good_model = model
+                        last_good_score = score
+                        last_good_n_estimators = n_estimators
+                    else:
+                        last_bad_n_estimators = n_estimators
 
-            last_bad_max_depth = 3
-            while True:
-                max_depth = round((last_good_max_depth+last_bad_max_depth) / 2)
-                if max_depth in [last_good_max_depth, last_bad_max_depth]:
-                    break
-                print("Trying depth {}".format(max_depth))
-                mape, model = self.__get_mape(cv_k, last_good_n_estimators, max_depth)
-                last_score = mape.mean()
-                if last_score < max_score:
-                    last_good_model = model
-                    last_good_score = mape
-                    last_good_max_depth = max_depth
-                else:
-                    last_bad_max_depth = max_depth
-                
-            last_good_model.fit(self.X, self.y, sample_weight=self.weights)
-            # last_good_model.fit(self.X, self.y)
-            spinner.ok()
+                last_bad_max_depth = 3
+                while True:
+                    max_depth = round((last_good_max_depth+last_bad_max_depth) / 2)
+                    if max_depth in [last_good_max_depth, last_bad_max_depth]:
+                        break
+                    print("Trying depth {}".format(max_depth))
+                    score, model = self.__get_score(cv_k, last_good_n_estimators, max_depth)
+                    last_score = score.mean()
+                    if last_score < max_score:
+                        last_good_model = model
+                        last_good_score = score
+                        last_good_max_depth = max_depth
+                    else:
+                        last_bad_max_depth = max_depth
+                    
+                spinner.ok()
+
+        # last_good_model.fit(self.X, self.y, sample_weight=self.weights)
+        last_good_model.fit(self.X, self.y)
 
         print("n_estimators={}; max_depth={}".format(last_good_n_estimators, last_good_max_depth))
-        print("Final cross-validation MAPE mean:    {}".format(round(last_good_score.mean(), 3)))
-        print("Final cross-validation MAPE std dev: {}".format(round(last_good_score.std(), 3)))
-        print("Final cross-validation MAPE min:     {}".format(round(last_good_score.min(), 3)))
-        print("Final cross-validation MAPE max:     {}".format(round(last_good_score.max(), 3)))
+        print("Final cross-validation RMSE mean:    {}".format(round(last_good_score.mean(), 3)))
+        print("Final cross-validation RMSE std dev: {}".format(round(last_good_score.std(), 3)))
+        print("Final cross-validation RMSE min:     {}".format(round(last_good_score.min(), 3)))
+        print("Final cross-validation RMSE max:     {}".format(round(last_good_score.max(), 3)))
         print("{}-fold base cross-validation scores:".format(cv_k))
         print(last_good_score)
 
